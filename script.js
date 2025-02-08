7786d31762555846a45d701b8e1408b7b2cb7879
Austin Gohn <atg50@pitt.edu>
2025-02-08 12:36:47 -0500

    Dynamic Math

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let userAns = "";
 const board = document.getElementById("placeholder-box");
 const answerBox = document.querySelector('.answer-box');
 const submitBtn = document.querySelector('.submit-btn');
+const emotionDisplay = document.getElementById("emotion-display");
 submitBtn.addEventListener('click', checkAnswer);
 const number1 = document.createElement('h3');
 const number2 = document.createElement('h3');
@@ -24,6 +25,15 @@ async function getEmotions() {
         //console.log('Response from Python:', data.dominant_emotion);
         currentEmotion = data.dominant_emotion
         console.log(data.dominant_emotion);
+        if (positiveEmotions.includes(currentEmotion))
+        {
+            emotionDisplay.textContent = `You're doing amazing!! Keep up the good work!`;
+        }
+        else
+        {
+            emotionDisplay.textContent = "It's ok to feel frustrated but I believe in you!";
+        }
+        
     } catch (error) {
         console.error('Error calling Python function:', error);
     }
@@ -35,9 +45,21 @@ function checkAnswer()
 
     if (userAns == ans)
     {
+        if (currentLevel <= 8 && positiveEmotions.includes(currentEmotion))
+        {
+            currentLevel += 2;
+        }
         answerBox.value = "";
         getNextProblem();
         displayProblem();
+    }
+    else if (NegativeEmotions.includes(currentEmotion) && currentLevel > 2)
+    {
+       currentLevel -= 2;
+    }
+    else if (PositiveEmotions.includes(currentEmotion) && currentLevel > 1)
+    {
+        currentLevel--;
     }
 }
 
