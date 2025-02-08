cf747bc9282aff7cec6136e5d6c94d24ab7779aa
Austin Gohn <atg50@pitt.edu>
2025-02-08 02:20:11 -0500

    emotion detection wip

--- EmotionDetection/Emotion.py
+++ EmotionDetection/Emotion.py
@@ -1,9 +1,40 @@
 from deepface import DeepFace
 import cv2
+import time
 
-#MonitorEmotion_From_Video(1, 'output.avi')
-vid = cv2.VideoCapture(0)
 
-while (tr)
+def get_emotions():
+    vid = cv2.VideoCapture(0)
+    frames_to_analyze = 15
+    frame_interval = 0.5
+    prev_time = time.time()
+    emotion_counts = {'angry': 0, 'happy': 0, 'neutral': 0, 'sad': 0, 'surprise': 0, 'disgust': 0, 'fear': 0}
 
+    while True:
+        res, frame = vid.read()
+
+        if res == False:
+            print("Could not capture fram")
+            break
+
+        current_time = time.time()
+
+        if current_time - prev_time >= frame_interval:
+            prev_time = current_time
+            result = DeepFace.analyze(frame, 'emotion', False)
+            cur_emotion = result[0]['dominant_emotion']
+        
+            if cur_emotion in emotion_counts:
+                emotion_counts[cur_emotion] += 1
+
+        if sum(emotion_counts.values()) == frames_to_analyze:
+            max_emotion = max(emotion_counts, key=emotion_counts.get)
+            max_count = emotion_counts[max_emotion]
+            emotion_counts = {emotion: 0 for emotion in emotion_counts}
+            print(f"'{max_emotion}' '{max_count}'")
+
+
+
+
+get_emotions()
 
